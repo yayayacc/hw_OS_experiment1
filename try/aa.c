8648d39e8bd84f67e309def419f0ed2c83d10380
yayayacc <1453991938@qq.com>
2022-10-21 15:57:57 +0800

    d

--- try/aa.c
+++ try/aa.c
@@ -2,6 +2,9 @@
 #include<stdio.h>
 #include<unistd.h>
 #include <wait.h>
+
+int my_int = 1;
+
 int main()
 {
     pid_t pid,pid1;
@@ -13,12 +16,16 @@ int main()
     else if ( pid == 0){
         pid1 = getpid();
         printf("child: pid = %d",pid);
-        printf("child: pid1 = %d",pid1);
+        printf("child: pid1 = %d\n",pid1);
+        my_int++;
+        printf("子进程中my_int的值：%d,地址为：%d\n",my_int,&my_int);
     }
     else{
         pid1 = getpid();
         printf("parent: pid = %d",pid);
-        printf("parent: pid1 = %d",pid1);
+        printf("parent: pid1 = %d\n",pid1);
+        my_int--;
+        printf("父进程中my_int的值：%d,地址为：%d\n",my_int,&my_int);
         wait(NULL);
     }
     return 0;
